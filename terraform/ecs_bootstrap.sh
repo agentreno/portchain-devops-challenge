deec7aa1217f906cff1161817162b4be52217aaa
Karl <kturrell@hotmail.com>
2021-02-03 14:09:15 +0000

    chore: setup EC2 capacity pool for ECS
    This gets the container running on an EC2 instance but hits a cloudwatch
    logging permission denied exception. IAM fixes to follow but the
    essential goal of running a container on AWS free tier infra is
    complete.

---
+++ terraform/ecs_bootstrap.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo ECS_CLUSTER=portchain >> /etc/ecs/ecs.config
--- terraform/main.tf
+++ terraform/main.tf
@@ -44,6 +44,70 @@ resource "aws_cloudwatch_log_group" "portchain_logs" {
 }
 
 # ECS
+data "aws_ami" "ecs_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-*-amazon-ecs-optimized"]
+  }
+}
+
+data "aws_iam_instance_profile" "ecs_instance_role" {
+  name = "ecsInstanceRole"
+}
+
+resource "aws_launch_template" "ecs_node" {
+  name_prefix   = "ecs"
+  image_id      = data.aws_ami.ecs_ami.image_id
+  instance_type = "t2.micro"
+
+  iam_instance_profile {
+    name = data.aws_iam_instance_profile.ecs_instance_role.name
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+  }
+
+  user_data = filebase64("ecs_bootstrap.sh")
+}
+
+resource "aws_autoscaling_group" "ecs_capacity" {
+  name                = "ecs-capacity-pool"
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+  # No scale-in/out behaviour for now
+  desired_capacity = 1
+  max_size         = 1
+  min_size         = 1
+
+  launch_template {
+    id      = aws_launch_template.ecs_node.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "AmazonECSManaged"
+    value               = ""
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_ecs_capacity_provider" "ec2" {
+  name = "ec2"
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn = aws_autoscaling_group.ecs_capacity.arn
+
+    managed_scaling {
+      # Start with no ECS scaling of the ASG, single-instance
+      status = "DISABLED"
+    }
+  }
+}
+
 resource "aws_ecs_cluster" "portchain" {
   name = "portchain"
 }
